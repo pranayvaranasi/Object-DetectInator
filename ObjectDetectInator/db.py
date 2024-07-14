c016adb2a7d58e30d6e1650d8c6e83e041f3b5c3
Pranay Varanasi <142153387+pranayvaranasi@users.noreply.github.com>
2024-07-14 21:27:13 +0530

    Update db.py

--- ObjectDetectInator/db.py
+++ ObjectDetectInator/db.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_pymongo import pymongo
-connection = #hided for privacy reasons
+connection = #hidden for privacy reasons
 client = pymongo.MongoClient(connection)
 db = client.get_database('ObjectDetectInator')
 collection = pymongo.collection.Collection(db, 'collection')
